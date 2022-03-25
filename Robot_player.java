b02f700e7ed2db6fcf2f89ddf100635932ae4640
anallain <99040327+Noiselis@users.noreply.github.com>
2022-03-25 09:18:21 +0100

    Docstring

--- Robot_player.java
+++ Robot_player.java
@@ -7,35 +7,34 @@ class Robot_player extends Player{
     }
 
 
-//Choisir un agent
-
+//Choisir un agent aléatoirement et retourner sa position
 public int[] select_choice(Vector <Agent> Agents){
     boolean alive = false;
     int[] pos = {-1,-1};
     while(!alive){
-        int max = Agents.size();            // Défini le maximu pour le nombre aléatoire à la taille du vecteur Agent
+        int max = Agents.size();
         Random ran = new Random();
-        int nb = ran.nextInt(max);          // Donne un entier aléatoire nb entre 0 et max
-        Agent selected = Agents.get(nb);    // Sélectionne l'Agent à la position nb
-        alive = selected.isAlive();         // Cherche si l'Agent est vivant
-        pos = selected.position();          // Prend la position de l'Agent sélectionné
+        int nb = ran.nextInt(max);
+        Agent selected = Agents.get(nb);
+        alive = selected.isAlive();
+        pos = selected.position(); 
     }
     
-    return pos ;                            // Retourne la position de l'Agent sélectionné
+    return pos ;
 }
 
+// Choisir le déplacement de l'Agent aléatoirement
 public String move_choice(Agent selected){
     Random ran = new Random();
-    int nb = ran.nextInt(4);                // Donne un entier aléatoire entre 0 et 3 inclus
+    int nb = ran.nextInt(4);
     switch (nb){
-        case 0 : {return "z";}              // Si le nombre aléatoire est 0, l'Agent va vers le haut
-        case 1 : {return "s";}              // Si le nombre aléatoire est 1, l'Agent va vers le bas
-        case 2 : {return "d";}              // Si le nombre aléatoire est 2, l'Agent va vers la droite
-        case 3 : {return "q";}              // Si le nombre aléatoire est 3, l'Agent va vers la gauche
+        case 0 : {return "z";}
+        case 1 : {return "s";}
+        case 2 : {return "d";} 
+        case 3 : {return "q";}
     }
-    return "Error";                         // Retour par défaut
+    return "Error"; 
 }
-
 
 
 }
