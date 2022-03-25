310b7f6afcf69781eac125769a809987464d3a59
Bordeterre <nikonekoelgato@gmail.com>
2022-03-25 10:46:41 +0100

    Update Agent.java

--- Agent.java
+++ Agent.java
@@ -2,50 +2,45 @@
 import java.io.*;
 import java.util.*;
 abstract class Agent{
-    public static int nb_agents = 0;
-
-    private int id;
     private boolean alive = true;
     private int x;
     private int y;
     private String icon;
     
-    // Création de l'agent et sa modification lorsqu'il bouge
-    public Agent (int pos_x, int pos_y, String icon){
-        id = nb_agents; 
-        nb_agents += 1;  
+    // Création de l'agent
+    public Agent (int pos_x, int pos_y, String icon){ 
         x = pos_x; 
         y = pos_y; 
         this.icon = icon;                                           
-    }  
-    // Change l'icone de l'Agent lorsqu'il est infecté ou guéri
+    } 
+    
+    // Change l'icone de l'Agent (par exemple, quand une cellule X devient Y, ou quand un virus infecte un cellule)
     public void setIcon(String new_icon){
         icon = new_icon;
     }
-    // Ajouter les positions x et y dans un tableau
-    public void setPosition(int[] pos){
-        x = pos[0];
-        y = pos[1];
-    } 
 
-    // Publier paramètres 
+    // Renvoie la position de l'agent 
     public int[] position(){
         int[] pos = {x,y};
         return pos;
     }
-    //Retourner l'icone de l'Agent
+    
+    //Renvoie l'icone de l'Agent
     public String display(){
         return icon;
     }
-    // Retourner true si l'Agent est vivant, false sinon
+    
+    // Renvoie true si l'Agent est vivant, false sinon
     public boolean isAlive(){
         return alive;
     }
 
     abstract void debug();
 
-    // Destruction : changer les coordonnées de l'Agent pour le déplacer en dehors du plateau
+    
     abstract void death();
+    
+    // Destruction : change les coordonnées de l'Agent pour le déplacer en dehors du plateau. Appelé par death()
     public void true_death(){
         x = -1;
         y = -1;
